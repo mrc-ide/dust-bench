312f8e53b414f901e599ca04ec2e5ce83f05df3a
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2021-09-01 13:38:26 +0100

    Fix filter profile script

--- profile_filter.R
+++ profile_filter.R
@@ -2,31 +2,30 @@ source("R/common.R")
 
 main <- function(args = commandArgs(TRUE)) {
   "Usage:
-bench_data.R [options]
+bench_data.R <data_type> [options]
 
 Options:
---real-data      Use real data
 --n-registers=N  Number of registers
 --n-particles=N  Number of particles
 --block-size=N   Block size" -> usage
   opts <- docopt::docopt(usage, args)
-  real_data <- opts$real_data
+  data_type <- opts$data_type
   n_registers <- as.integer(opts$n_registers)
   n_particles <- as.integer(opts$n_particles)
   block_size <- as.integer(opts$block_size)
 
-  do_data(real_data, n_registers, block_size, n_particles)
+  do_filter(data_type, n_registers, block_size, n_particles)
 }
 
 
-do_filter <- function(real_data, n_registers, block_size, n_particles) {
-  n_vacc_classes <- if (real_data) 4L else 1L
+do_filter <- function(data_type, n_registers, block_size, n_particles) {
+  n_vacc_classes <- if (data_type == "real") 4L else 1L
   gen <- model_gpu_create("carehomes", n_registers,
                           n_vacc_classes = n_vacc_classes)
 
   device_config <- list(device_id = 0L, run_block_size = block_size)
   dat <- create_filter(gen, n_particles,
-                       real_data = real_data,
+                       data_type = data_type,
                        device_config = device_config,
                        n_threads = 10)
 
